00315bbae1315f7ce0dc93eb730038befd423f9d
Samindi Ratnayake <samindiratnayake@Samindis-MBP.modem>
2025-01-24 21:35:32 +1100

    Added position logic for each player

--- lib/player.rb
+++ lib/player.rb
@@ -1,10 +1,4 @@
-# Things to consider
-
-# position (where is the player at the moment)
-# money: $16, lose money by paying rent(double if owner owns all the colors), buying properties, passing go  :earn money by getting paid rent
-# properties (what properties does the player own?? )
-
-
+# do a comment 
 class Player
     attr_accessor :name, :money, :position, :properties
     def initialize(name)
--- main.rb
+++ main.rb
@@ -1,68 +1,72 @@
-# will contain the main method that calls the other objects
-
-#for each dice roll, add the position of the player until it reaches 10. 
-
+require 'ostruct'
 require 'json'
+require 'colorize'
 require_relative 'lib/player'
 
-
-require 'ostruct'
-
+#loading the board
 board_file = 'data/board.json'
 board = JSON.parse(File.read(board_file), object_class: OpenStruct)
 
+# adding new attributes to the board: property owners and owned conditions( is it owned or not)
 for i in 1..(board.length-1) do
     board[i].owned = false
-    board[i].owner = null
+    board[i].owner = -1
 end
 
-
-roll_file = 'data/rolls_1.json'
+#loading the roll file
+roll_file = 'data/rolls_2.json'
 rolls = JSON.parse(File.read(roll_file))
 
 #setting up the players
 player1 = Player.new("Peter")
 player2 = Player.new("Billy")
 player3 = Player.new("Charlotte")
-player4 = Player.new("Swedal")
-   
+player4 = Player.new("Sweedal")
+
+#load the players to an array
 players = [player1, player2, player3, player4]
 
+
+#current player index
 i = 0
+#current dice roll index
 x = 0
-while players[i].money > 0
+
+# this loop will go through each player, but will stop when the players money reaches 0-- might change this so its not like an infiite loop
+# while players[i].money > 0 
+while x < 10
+    # increment the players position based on the roll. 
     players[i].position += rolls[x]
+    
+    #what to do if the player's position exceeds the board length? Player is on position 8 but the board is only a length of 5
+    #maybe we can find the difference and make that the players new position???
 
     if players[i].position >= board.length
         difference = players[i].position - board.length 
         players[i].position = difference
     end
 
-    if board[player[i].position].owned == false
-        if player[i]money >= board[player[i].position].price
-            player[i].money -= board[player[i].price]
-            board[player[i].position].owned = true
-            player[i].properties << board[player[i].position].name
-            board[player[i].position].owner = i
-        end
-
-    else 
-    #for each property in the current position, reduce money from player based on price
-    #if property owned = false and we have money then reduce from player 
-
-    # players[i].money -= 5
     puts players[i].position
     puts players[i].name
-
-    #loops the players
+    #counts to increment the index and the rolls 
     x += 1
     i += 1 
+
+    #makes sure that there are turns: like go from player1 to 4 and then back to 1...
     if i == 4
         i = 0
     end
+
 end
 
 
+#to dos
 
+#Changes to Money, Ownership
+    #what happens if player lands on GO!: Need to increase their money by $1
+    #if the board is not owned? they must buy it!! So reduce their money by the price. Append their properties array? Maybe I can print at the end
+    # change the owned == false to true so no one else can buy it!!
 
-
+    # if the board is owned? We need to pay rent. This money needs to go to the owner of that property (we need a attribute in board called owner)
+    # also the current players money needs to reduce by the price (i decided maybe to use the property price as the rent price because its already low)
+    #if the money is less than rent? Maybe we can like go bankrupt and say game over bcs technically they dont have money to pay rent! Game over and exit the loop??
